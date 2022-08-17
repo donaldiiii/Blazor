b51ba54aca40fd01d21a4b197d763d2885b67d66
Donald Musabelliu <donaldmusabelliu@gmail.com>
2022-08-17 09:11:33 +0200

    Add project files.

---
+++ BlazorMovies/Shared/Entities/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Shared.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Enter Custom Validation Message")]
+        public string Name { get; set; }
+    }
+}
